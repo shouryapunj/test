68ec316e932100c769775259d941b82f105992ba
Shourya Punj <shourya.punj@ofbusiness.in>
2017-08-02 18:27:43 +0530

    query Partial Search

---
+++ src/server.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './app';
+import template from './template';
+//import topics1 from './topics1.json';
+import request from 'request';
+
+const server = express();
+
+server.use('/assets', express.static('assets'));
+
+server.get('/', (req, res) => {
+
+// var x = req.url + "?pageSize=4";
+//var y = "https://api.myjson.com/bins/15rw33";
+//var y = req.query;
+//console.log()
+//var x = JSON.stringify(req.query);
+var x = req.query;
+console.log(x);
+//var y = x.Name;
+//var y = JSON.stringify(x);
+//var z = x + y;
+//console.log(x);
+//console.log(x);
+var uri = "https://cipherapi.ofbusiness.com/api/tender/tenders?query=";
+var y = "pageSize=10";
+//console.log(req.query);
+
+
+  if (req.query.query) {
+    uri += req.query.query;
+  }
+  // else{
+  // uri += y;
+  // }
+
+
+console.log('URL', uri);
+request({
+ //uri: "https://api.myjson.com/bins/15rw33" + x,
+ uri: uri,
+ method: "GET",
+ timeout: 10000,
+}, function(error,response,body){
+ body = JSON.parse(body);
+//console.log(body);
+
+ const isMobile = true;
+//console.log(body);
+var content = [];
+if (body.data) {
+content = body.data.content;
+}
+ const initialState = { isMobile, topics: content }; 
+ const appString = renderToString(<App {...initialState} />);
+//console.log(body.data);
+//console.log(body.data.content);
+ res.send(template({
+ body: appString,
+ title: 'Pagination',
+ initialState: JSON.stringify(initialState)
+}));
+console.log(res.data);
+});
+})
+
+
+server.listen(8080);
+console.log('listening');
+//var x = window.location.hostname();
+//console.log(x);
---
+++ src/template.js
@@ -0,0 +1,42 @@
+export default ({ body, title, initialState }) => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <script>window.__APP_INITIAL_STATE__ = ${initialState}</script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src = "http://fb.me/JSXTransformer-0.8.0.js"></script>
+        <script src = "http://fb.me/react-0.8.0.js"></script>
+        <title>${title}</title>
+        <style>
+         .pageLink { cursor: pointer; text-decoration: underline; color: blue; }
+    table { border-spacing: 4px; border-collapse: collapse; }
+    td, th { border: 1px solid black; padding: 4px 8px; } 
+    .button1 {
+      align: center;
+    border-radius: 2px;
+    background-color: lightgreen;
+    border: none;
+    color: black;
+    padding: 2px 6px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 12px;
+    margin: 4px 2px;
+    cursor: pointer;
+     }
+    </style>
+      </head>
+      
+      <body>
+        <div id="root">${body}</div>
+      </body>
+      
+      <script src="/assets/bundle.js"></script>
+    </html>
+  `;
+};
